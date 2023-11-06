e2efd1a752c44c351002e69135b1a834f396db7a
ffnew <cloudmass9@gmail.com>
2023-11-06 19:37:41 +0800

    Disable log

--- library/custom_logging.py
+++ library/custom_logging.py
@@ -17,28 +17,28 @@ def setup_logging(clean=False, debug=False):
     if log is not None:
         return log
     
-    # try:
-    #     if clean and os.path.isfile('setup.log'):
-    #         os.remove('setup.log')
-    #     time.sleep(0.1) # prevent race condition
-    # except:
-    #     pass
+    # # try:
+    # #     if clean and os.path.isfile('setup.log'):
+    # #         os.remove('setup.log')
+    # #     time.sleep(0.1) # prevent race condition
+    # # except:
+    # #     pass
     
-    if sys.version_info >= (3, 9):
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(pathname)s | %(message)s', filemode='a', encoding='utf-8', force=True)
-    else:
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(pathname)s | %(message)s', filemode='a', force=True)
+    # if sys.version_info >= (3, 9):
+    #     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(pathname)s | %(message)s', filemode='a', encoding='utf-8', force=True)
+    # else:
+    #     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(pathname)s | %(message)s', filemode='a', force=True)
     
-    console = Console(log_time=True, log_time_format='%H:%M:%S-%f', theme=Theme({
-        "traceback.border": "black",
-        "traceback.border.syntax_error": "black",
-        "inspect.value.border": "black",
-    }))
-    pretty_install(console=console)
-    traceback_install(console=console, extra_lines=1, width=console.width, word_wrap=False, indent_guides=False, suppress=[])
-    rh = RichHandler(show_time=True, omit_repeated_times=False, show_level=True, show_path=False, markup=False, rich_tracebacks=True, log_time_format='%H:%M:%S-%f', level=logging.DEBUG if debug else logging.INFO, console=console)
-    rh.set_name(logging.DEBUG if debug else logging.INFO)
+    # console = Console(log_time=True, log_time_format='%H:%M:%S-%f', theme=Theme({
+    #     "traceback.border": "black",
+    #     "traceback.border.syntax_error": "black",
+    #     "inspect.value.border": "black",
+    # }))
+    # pretty_install(console=console)
+    # traceback_install(console=console, extra_lines=1, width=console.width, word_wrap=False, indent_guides=False, suppress=[])
+    # rh = RichHandler(show_time=True, omit_repeated_times=False, show_level=True, show_path=False, markup=False, rich_tracebacks=True, log_time_format='%H:%M:%S-%f', level=logging.DEBUG if debug else logging.INFO, console=console)
+    # rh.set_name(logging.DEBUG if debug else logging.INFO)
     log = logging.getLogger("sd")
-    log.addHandler(rh)
+    # log.addHandler(rh)
     
     return log
