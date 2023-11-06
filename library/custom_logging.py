feb586c90b7268623e274d50ea03e3ef0e24b55e
ffnew <cloudmass9@gmail.com>
2023-11-06 17:45:45 +0800

    Fix logging

--- library/custom_logging.py
+++ library/custom_logging.py
@@ -9,13 +9,15 @@ from rich.console import Console
 from rich.pretty import install as pretty_install
 from rich.traceback import install as traceback_install
 
+from common.log import logger
+
 log = None
 
 def setup_logging(clean=False, debug=False):
-    global log
+    # global log
     
-    if log is not None:
-        return log
+    # if log is not None:
+    #     return log
     
     # # try:
     # #     if clean and os.path.isfile('setup.log'):
@@ -38,7 +40,7 @@ def setup_logging(clean=False, debug=False):
     # traceback_install(console=console, extra_lines=1, width=console.width, word_wrap=False, indent_guides=False, suppress=[])
     # rh = RichHandler(show_time=True, omit_repeated_times=False, show_level=True, show_path=False, markup=False, rich_tracebacks=True, log_time_format='%H:%M:%S-%f', level=logging.DEBUG if debug else logging.INFO, console=console)
     # rh.set_name(logging.DEBUG if debug else logging.INFO)
-    log = logging.getLogger("sd")
+    # log = logging.getLogger("sd")
     # log.addHandler(rh)
     
-    return log
+    return logger
